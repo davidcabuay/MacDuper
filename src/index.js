4cba2e1fc271048d1fc068cbf93e562065c789fa
David <david.cabuay@gmail.com>
2023-02-08 20:04:41 -0800

    Created assets folder

--- src/index.js
+++ src/index.js
@@ -10,169 +10,169 @@ const  itemArr = [
         name: 'screwdriver',
         txt: 'I turn and twist, with a handle to grip, With screws I work to tighten or strip',
         // img: 'https://picsum.photos/200/301',
-        img: 'src/scripts/screwdriver.png',
+        img: 'assets/screwdriver.png',
         id: 0
     },
     {
         name: 'copper wire',
         txt: 'I am thin, I am red, Electricity, I carry ahead',
-        img: 'src/scripts/copperwire.png',
+        img: 'assets/copperwire.png',
         id: 1
     } ,
     {
         name: 'sharpie',
         txt: 'I am used for art, or just to jot, A permanent marker is what I get called a lot!',
-        img: 'src/scripts/sharpie.png',
+        img: 'assets/sharpie.png',
         id: 2
     },
     {
         name: 'paper clip',
         txt: 'Papers and notes, I keep in line, A humble helper all the time',
-        img: 'src/scripts/paperclip.png',
+        img: 'assets/paperclip.png',
         id: 3
     },
     {
         name: 'paper',
         txt: 'I am usually white but now I am scruched tight, I am the canvas you write on at night',
-        img: 'src/scripts/paper.png',
+        img: 'assets/paper.png',
         id: 4
     },
     {
         name: 'toothpaste',
         txt: 'I come in a tube, and i foam and I squirt, I keep your teeth clean and remove the dirt',
-        img: 'src/scripts/toothpaste.png',
+        img: 'assets/toothpaste.png',
         id: 5
     },
     {
         name: 'toothbrush',
         txt: 'With bristles firm and clean, I keep your teeth serene',
-        img: 'src/scripts/toothbrush.png',
+        img: 'assets/toothbrush.png',
         id: 6
     },
     {
         name: 'quarter',
         txt: 'I am round and shiny with a picture or two, In wallets and purses you can find my crew',
-        img: 'src/scripts/coin.png',
+        img: 'assets/coin.png',
         id: 7
     },
     {
         name: 'tide pod',
         txt: 'I have many colors, but I am not to eat, I am used for the laundry to clean your sheets',
-        img: 'src/scripts/tidepod.png',
+        img: 'assets/tidepod.png',
         id: 8
     },
     {
         name: 'pizza',
         txt: 'With cheese, sause and dough, I am complete, A favorite food you can not wait to eat',
-        img: 'src/scripts/pizza.png',
+        img: 'assets/pizza.png',
         id: 9
     },
     {
         name: 'loose thread',
         txt: 'I am thin and long and can be spun, With needle and cloth I would not be undone',
-        img: 'src/scripts/thread.png',
+        img: 'assets/thread.png',
         id: 10
     },
     {
         name: 'hotdog',
         txt: 'I am long in a bun and often called a glizzy, you can eat me on the go whenever you are busy',
-        img: 'src/scripts/hotdog.png',
+        img: 'assets/hotdog.png',
         id:11
     },
     // {
     //     name: 'chapstick',
     //     txt: 'chapstick',
-    //     img: 'src/scripts/chapstick.png',
+    //     img: 'assets/chapstick.png',
     //     id: 12
     // },
     {
         name: 'clothespin',
         txt: 'I am small and wooden, with two arms and a spring, I clip clothes to the line and let the breeze make them swing ,',
-        img: 'src/scripts/clothespin.png',
+        img: 'assets/clothespin.png',
         id: 13
     },
     {
         name: 'comb',
         txt: 'I have teeth that are fine and sturdy, I comb your hair to make you look thirty',
-        img: 'src/scripts/comb.png',
+        img: 'assets/comb.png',
         id: 14
     },
     {
         name: 'feather',
         txt: 'From birds I come to keep them warm, I am used for writing and decoration and even to peform',
-        img: 'src/scripts/feather.png',
+        img: 'assets/feather.png',
         id: 15
     },
     {
         name: 'fork',
         txt: 'I have tines that are sharp and slick, With me you eat, no chopsticks',
-        img: 'src/scripts/fork.png',
+        img: 'assets/fork.png',
         id: 16
     },
     {
         name: 'gum',
         txt: 'I am chewy and sticky, and I fight halitosis, I freshen your breath for your awaited kiss',
-        img: 'src/scripts/gum.png',
+        img: 'assets/gum.png',
         id: 17
     },
     {
         name: 'pills',
         txt: 'I am small and oblong and filled with surprise, With medicine or vitamins, I protect you from demise ',
-        img: 'src/scripts/pills.png',
+        img: 'assets/pills.png',
         id: 18
     },
     {
         name: 'rubberband',
         txt: 'I am stretchy and strong, and come in a band, I hold things together, under your command ',
-        img: 'src/scripts/rubberband.png',
+        img: 'assets/rubberband.png',
         id: 19
     },
     {
         name: 'scissors',
         txt: 'I have two blades, that can be used for trimming, I cut paper or fabric, do not use me for killing',
-        img: 'src/scripts/scissors.png',
+        img: 'assets/scissors.png',
         id: 20
     },
     {
         name: 'scrunchie',
         txt: 'I am soft and stretchy, and come in a knot, I keep your hair back, to hide your bald spot',
-        img: 'src/scripts/scrunchie.png',
+        img: 'assets/scrunchie.png',
         id: 21
     },
     {
         name: 'spoon',
         txt: 'I have a bowl, and a handle so long, if you think I am a fork then you are wrong',
-        img: 'src/scripts/spoon.png',
+        img: 'assets/spoon.png',
         id: 22
     },
     {
         name: 'spring',
         txt: 'I am coiled and tight and made of metal, with tension and force stronger than a boiling kettle',
-        img: 'src/scripts/spring.png',
+        img: 'assets/spring.png',
         id: 23
     },
     {
         name: 'thermometer',
         txt: 'I measure heat and come in many forms, Like oral or ear, I am here to inform',
-        img: 'src/scripts/thermometer.png',
+        img: 'assets/thermometer.png',
         id: 24
     },
     {
         name: 'wrench',
         txt: 'I am a tool, that loosens and tightens, with nuts and bolts, then and again ',
-        img: 'src/scripts/wrench.png',
+        img: 'assets/wrench.png',
         id: 25
     },
     {
         name: 'pen',
         txt: 'I have a point, that writes on paper, with a blob of ink, you are my maker ',
-        img: 'src/scripts/pen.png',
+        img: 'assets/pen.png',
         id: 26
     },
     {
         name: 'brush',
         txt: 'I have bristles, straight  and in angles, with the knots in your hair, I can help you de-tangle',
-        img: 'src/scripts/brush.png',
+        img: 'assets/brush.png',
         id: 27
     }
 
@@ -180,33 +180,33 @@ const  itemArr = [
 
 
 const backMusic = new Audio()
-backMusic.setAttribute('src', "src/scripts/backmusic.mp3");
+backMusic.setAttribute('src', "assets/backmusic.mp3");
 const twentys = new Audio()
-twentys.setAttribute('src', "src/scripts/test20.mp3");
+twentys.setAttribute('src', "assets/test20.mp3");
 const tens = new Audio()
-tens.setAttribute('src', "src/scripts/10good.mp3");
+tens.setAttribute('src', "assets/10good.mp3");
 const fifteens = new Audio()
-fifteens.setAttribute('src', "src/scripts/15good.mp3");
+fifteens.setAttribute('src', "assets/15good.mp3");
 const fives = new Audio()
-fives.setAttribute('src', "src/scripts/5good.mp3");
+fives.setAttribute('src', "assets/5good.mp3");
 const explosion = new Audio()
-explosion.setAttribute('src', "src/scripts/explosion.mp3");
+explosion.setAttribute('src', "assets/explosion.mp3");
 const victory = new Audio()
-victory.setAttribute('src', "src/scripts/victory.mp3");
+victory.setAttribute('src', "assets/victory.mp3");
 victory.volume = 0.2;
 const noreal = new Audio()
-noreal.setAttribute('src', "src/scripts/noreal.mp3");
+noreal.setAttribute('src', "assets/noreal.mp3");
 const congreal = new Audio()
-congreal.setAttribute('src', "src/scripts/congreal.mp3");
+congreal.setAttribute('src', "assets/congreal.mp3");
 const missImp = new Audio()
-missImp.setAttribute('src', "src/scripts/MissImp.mp3");
+missImp.setAttribute('src', "assets/MissImp.mp3");
 missImp.volume = 0.1;
 const nice = new Audio()
-nice.setAttribute('src', "src/scripts/nice.mp3");
+nice.setAttribute('src', "assets/nice.mp3");
 const omgwedie = new Audio()
-omgwedie.setAttribute('src', "src/scripts/omgwedie.mp3");
+omgwedie.setAttribute('src', "assets/omgwedie.mp3");
 const death = new Audio()
-death.setAttribute('src', "src/scripts/death.mp3");
+death.setAttribute('src', "assets/death.mp3");
 
 const muteButton = document.getElementById("mute");
 
@@ -419,7 +419,7 @@ const startpage = document.getElementById("startpage");
 function startGame(){
     //remove the startscreen
     startpage.setAttribute("hidden", true)
-    document.body.style.backgroundImage = "url('src/scripts/background.png')"
+    document.body.style.backgroundImage = "url('assets/background.png')"
     gamepage.removeAttribute("hidden")
     //removechild
     //coundown 3,2,1 print on screen
